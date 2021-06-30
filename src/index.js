c6761f53815fa123bff10934cd55d6f861eb10a4
xarrijorge <kodinglife@gmail.com>
2021-06-30 14:36:13 +0000

    Remove firebase config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import firebase from 'firebase'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: 'AIzaSyCUVCbfPbEHTcP45Xjc4aly2XxnAxli1eo',
-    authDomain: 'cfbookmentor.firebaseapp.com',
-    projectId: 'cfbookmentor',
-    databaseURL:
-        'https://cfbookmentor-default-rtdb.europe-west1.firebasedatabase.app/',
-    storageBucket: 'cfbookmentor.appspot.com',
-    messagingSenderId: '229496005028',
-    appId: '1:229496005028:web:76579244b94ca14f45e131',
-}
-
-firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
     <React.StrictMode>
